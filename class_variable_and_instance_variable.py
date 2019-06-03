c6e0325245b47a06a5c298d09ac604b870aa8130
heshengjin[何胜金] <2356899074@qq.com>
2019-06-03 18:00:18 +0800

    Update class_variable_and_instance_variable.py

--- class_variable_and_instance_variable.py
+++ class_variable_and_instance_variable.py
@@ -2,6 +2,32 @@
 # -*- coding:UTF-8 -*-
 """
 python的类变量和成员变量
+python的类变量和成员变量
+类似于JavaScript方式，类属性和类方法就在prototype上面
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>JavaScript测试类属性和实例对象属性</title>
+	</head>
+	<body>
+	<script type="text/javascript">
+		function test(){
+           //TODO
+		}
+       test.prototype.nameTest = 'hsj-create-2019/06/03';
+	   //undefined
+	   console.log(test.nameTest);
+	   var instanceTest = new test();
+	   //hsj-create-2019/06/03
+	   console.log(instanceTest.nameTest);
+	   instanceTest.nameTest = 'hsj-create-2019/06/03-new';
+	   //hsj-create-2019/06/03-new
+	   console.log(instanceTest.nameTest);
+	   //hsj-create-2019/06/03
+	   console.log(instanceTest.__proto__.nameTest);
+	</script>
+</html>
 """
 
 
